ee775f713c7bab4bba3223bcb8e8ca79930b21de
Abassbako <abassbako22@gmail.com>
2023-09-29 01:07:22 +0100

    MongoDB CRUD Operation

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const booksRoute = require('./routes/book');
+const booksRoute = require('../routes/book');
 
 const { ObjectId } = require('mongodb');
 
